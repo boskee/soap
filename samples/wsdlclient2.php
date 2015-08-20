6fc7ba4b6bf8798a5257f5261af450a3476bcdcd
Wojciech Zylinski <wzylinski@splashworldwide.com>
2015-08-20 23:18:24 +0100

    Use magic __call to handle WSDL methods instead of eval in Soap\Client

--- samples/wsdlclient2.php
+++ samples/wsdlclient2.php
@@ -23,7 +23,6 @@ if ($err) {
 	exit();
 }
 $client->setUseCurl($useCURL);
-$proxy = $client->getProxy();
 $param = array(
 	'browse_node' => 18,
 	'page' => 1,
@@ -32,15 +31,15 @@ $param = array(
 	'type' => 'lite',
 	'devtag' => 'Your dev tag'
 );
-$result = $proxy->BrowseNodeSearchRequest($param);
+$result = $client->BrowseNodeSearchRequest($param);
 // Check for a fault
-if ($proxy->fault) {
+if ($client->fault) {
 	echo '<h2>Fault</h2><pre>';
 	print_r($result);
 	echo '</pre>';
 } else {
 	// Check for errors
-	$err = $proxy->getError();
+	$err = $client->getError();
 	if ($err) {
 		// Display the error
 		echo '<h2>Error</h2><pre>' . $err . '</pre>';
@@ -51,8 +50,6 @@ if ($proxy->fault) {
 		echo '</pre>';
 	}
 }
-echo '<h2>Request</h2><pre>' . htmlspecialchars($proxy->request, ENT_QUOTES) . '</pre>';
-echo '<h2>Response</h2><pre>' . htmlspecialchars($proxy->response, ENT_QUOTES) . '</pre>';
+echo '<h2>Request</h2><pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
+echo '<h2>Response</h2><pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
 echo '<h2>Client Debug</h2><pre>' . htmlspecialchars($client->debug_str, ENT_QUOTES) . '</pre>';
-echo '<h2>Proxy Debug</h2><pre>' . htmlspecialchars($proxy->debug_str, ENT_QUOTES) . '</pre>';
-?>
